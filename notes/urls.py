20543c9043e295d6e44b1e1943d805a39ef3cb07
Emile <shumbushoemilef@gmail.com>
2021-11-21 11:34:18 +0200

    urls"

--- notes/urls.py
+++ notes/urls.py
@@ -2,23 +2,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+# from rest_framework import permissions
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Recipe API",
-      default_version='Version 1.0',
-      description="THis API Created only for learning purpose",
-      terms_of_service="Trems of services Will be published later ",
-      contact=openapi.Contact(email="shumbushoemilef@gmail.com"),
-      license=openapi.License(name="Emile License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-   authentication_classes=[]
-)
+# schema_view = get_schema_view(
+#    openapi.Info(
+#       title="Recipe API",
+#       default_version='Version 1.0',
+#       description="THis API Created only for learning purpose",
+#       terms_of_service="Trems of services Will be published later ",
+#       contact=openapi.Contact(email="shumbushoemilef@gmail.com"),
+#       license=openapi.License(name="Emile License"),
+#    ),
+#    public=True,
+#    permission_classes=(permissions.AllowAny,),
+#    authentication_classes=[]
+# )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
